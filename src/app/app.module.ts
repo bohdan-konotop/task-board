53ae1b6084518f52785d2a01cbd66c589ab5e766
Quadross <elektrogladiator@gmail.com>
2022-07-18 17:53:45 +0300

    add template of modal window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainModule } from './main/main.module';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ModalComponent],
   imports: [BrowserModule, MainModule],
   providers: [],
   bootstrap: [AppComponent],
